dd4e52c1037172c1f742e814e0f9ae6abbe605d4
FilipWirsen <Filip.wirsen99@gmail.com>
2022-02-24 12:51:23 +0000

    Add function to get player input and add their symbol to the board

--- run.py
+++ run.py
@@ -4,6 +4,9 @@ BOARD = ["-", "-", "-",
         "-", "-", "-",
         "-", "-", "-"]
 
+run_game = True
+current_player = "X"
+
 #print board
 def print_board(board):
     """
@@ -14,10 +17,22 @@ def print_board(board):
     print(board[6] + "|" + board[7] + "|" + board[8])
 
 #Player input 
+def player_input(board, player):
+    """
+    Take player input and add their symbol to the board
+    """
+    inp = int(input(f"{player} please chose a number between 1-9 to place your symbol: \n"))
+    board[inp-1] = current_player
 
 #Check for win or tie 
 
 #Switch player
 
 #Run game
-print_board(BOARD)
+player_one = input("Player one, please enter your name: \n")
+player_two = input("Player two, please enter your name: \n")
+while run_game:
+    player_input(BOARD, player_one)
+    print_board(BOARD)
+    player_input(BOARD, player_two)
+    print_board(BOARD)
