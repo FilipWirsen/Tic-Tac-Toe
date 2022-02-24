6668fbe63b9937a826289bd95029f8da8f675e3a
FilipWirsen <Filip.wirsen99@gmail.com>
2022-02-24 11:30:46 +0000

    initial commit

--- run.py
+++ run.py
@@ -1,3 +1,21 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# Create Board
+
+
+
+# Create player names
+
+
+
+# Player input
+
+
+
+# Check for win and tie 
+
+
+
+# Switch player
+
+
+
+# Run game
