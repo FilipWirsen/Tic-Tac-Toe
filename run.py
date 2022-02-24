18cc74c9566a42313f8c5a35f338503e0f197a18
FilipWirsen <Filip.wirsen99@gmail.com>
2022-02-24 12:44:09 +0000

    Add function to print board

--- run.py
+++ run.py
@@ -1,21 +1,23 @@
-# Create Board
+# Create board
 
+BOARD = ["-", "-", "-",
+        "-", "-", "-",
+        "-", "-", "-"]
 
+#print board
+def print_board(board):
+    """
+    Prints the board 
+    """
+    print(board[0] + "|" + board[1] + "|" + board[2])
+    print(board[3] + "|" + board[4] + "|" + board[5])
+    print(board[6] + "|" + board[7] + "|" + board[8])
 
-# Create player names
+#Player input 
 
+#Check for win or tie 
 
+#Switch player
 
-# Player input
-
-
-
-# Check for win and tie 
-
-
-
-# Switch player
-
-
-
-# Run game
+#Run game
+print_board(BOARD)
